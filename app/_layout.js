0ee8b5128874a60277d69477e33697b8abc57f3a
Lalit sharma <geekymonkcp742k@gmail.com>
2023-03-24 01:13:31 +0530

    Popular and Nearby jobs fetched and shown on UI

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,28 @@
-import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 
+import { Stack } from "expo-router";
+// importing CUSTOMFONT for that we need to import useCallback hook and useFonts from expo-font library
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 const Layout = () => {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    // we want to show our Home Page if the fonts happens to loaded
+
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+  return <Stack onLayout={onLayoutRootView} />;
 };
 
 export default Layout;
